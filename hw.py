0ac103232efbc98d2ee926112141109a1887dc17
Nihanth2025 <sk1441@gmail.com>
2025-04-16 16:17:12 +0530

    Add files via upload

---
+++ hw.py
@@ -0,0 +1,7 @@
+cha = input("Enter the character: ")
+
+if len(cha) == 1:
+    ascii_value = ord(cha)
+    print("The ASCII value :", ascii_value)
+else:
+    print("Please enter only one character.")
